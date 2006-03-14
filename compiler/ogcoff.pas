64f01373cc808b5e317c05e7b8252136611d787b
peter <peter@3ad0048d-3df7-0310-abae-a5850022a9f2>
2006-03-14 06:45:15 +0000

    * data and bss start and end symbols
    git-svn-id: http://svn.freepascal.org/svn/fpc/branches/linker@2912 3ad0048d-3df7-0310-abae-a5850022a9f2

--- compiler/ogcoff.pas
+++ compiler/ogcoff.pas
@@ -2307,6 +2307,7 @@ const win32stub : array[0..131] of byte=(
             Concat('  SYMBOL etext');
             Concat('ENDEXESECTION');
             Concat('EXESECTION .data');
+            Concat('  SYMBOL __data_start__');
             Concat('  OBJSECTION .data*');
             Concat('  OBJSECTION .fpc_resstr_data*');
             Concat('  ALIGN 4');
@@ -2314,6 +2315,7 @@ const win32stub : array[0..131] of byte=(
             Concat('  OBJSECTION .fpc_resstr_index*');
             Concat('  ZEROS 16');
             Concat('  SYMBOL edata');
+            Concat('  SYMBOL __data_end__');
             Concat('ENDEXESECTION');
             Concat('EXESECTION .idata');
             Concat('  OBJSECTION .idata$2');
@@ -2325,7 +2327,9 @@ const win32stub : array[0..131] of byte=(
             Concat('  OBJSECTION .idata$7');
             Concat('ENDEXESECTION');
             Concat('EXESECTION .bss');
+            Concat('  SYMBOL __bss_start__');
             Concat('  OBJSECTION .bss*');
+            Concat('  SYMBOL __bss_end__');
             Concat('ENDEXESECTION');
             Concat('EXESECTION .rsrc');
             Concat('  OBJSECTION .rsrc*');
