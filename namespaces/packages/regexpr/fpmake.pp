39bbc133a6c546708620b342d0778a05ad5378a7
paul <paul@3ad0048d-3df7-0310-abae-a5850022a9f2>
2011-08-27 03:50:04 +0000

    git-svn-id: http://svn.freepascal.org/svn/fpc/branches/paul@18858 3ad0048d-3df7-0310-abae-a5850022a9f2

---
+++ namespaces/packages/regexpr/fpmake.pp
@@ -0,0 +1,33 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('regexpr');
+{$ifdef ALLPACKAGES}
+    P.Directory:='regexpr';
+{$endif ALLPACKAGES}
+    P.Version:='2.7.1';
+    P.SourcePath.Add('src');
+
+    T:=P.Targets.AddUnit('regexpr.pp');
+    T:=P.Targets.AddUnit('regex.pp');
+    T.ResourceStrings := True;
+
+    P.ExamplePath.Add('tests');
+    P.Targets.AddExampleProgram('testreg1.pp');
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
