adem0x/fpc

commit 9df1a57a8b367541608ac98abcc04083c2f28011

support SP and FP alias in the assembler reader too
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@27573 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/m68k/ra68kmot.pas
@@ -201,12 +201,26 @@ const
 
     function tm68kmotreader.is_register(const s:string):boolean;
       begin
-        is_register:=false;
-        // FIX ME!!! Ugly, needs a proper fix (KB)
+        result:=false;
+        // FIX ME!!! '%'+ is ugly, needs a proper fix (KB)
         actasmregister:=gas_regnum_search('%'+lower(s));
         if actasmregister<>NR_NO then
           begin
-            is_register:=true;
+            result:=true;
+            actasmtoken:=AS_REGISTER;
+          end;
+        { reg found?
+          possible aliases are always 2 char
+        }
+        if result or (length(s)<>2) then
+          exit;
+        if lower(s)='sp' then
+          actasmregister:=NR_STACK_POINTER_REG;
+        if lower(s)='fp' then
+          actasmregister:=NR_STACK_POINTER_REG;
+        if actasmregister<>NR_NO then
+          begin
+            result:=true;
             actasmtoken:=AS_REGISTER;
           end;
       end;
@@ -1410,7 +1424,6 @@ const
                               if expr = 'SELF' then
                                 oper.SetupSelf
                               else begin
-                                writeln('unknown id: ',expr);
                                 Message1(sym_e_unknown_id,expr);
                               end;
                               expr:='';
@@ -1459,7 +1472,6 @@ const
                   end;
    { // Register, a variable reference or a constant reference // }
      AS_REGISTER: begin
-//                   writeln('register! ',actasmpattern);
                    { save the type of register used. }
                    tempstr := actasmpattern;
                    Consume(AS_REGISTER);

commit 01985cb37135c903902fe21866dcba9922d6dfe7

* m68k: Fixed damage caused by r27573 and r28177:   * longjmp: restored loading of function result (d0) which got removed in r28177.   * While at the point, added test for zero, because longjmp must never return zero values.   * Assembler reader: 'fp' refers to frame pointer, not the stack pointer.
git-svn-id: http://svn.freepascal.org/svn/fpc/trunk@28183 3ad0048d-3df7-0310-abae-a5850022a9f2

compiler/m68k/ra68kmot.pas
@@ -220,7 +220,7 @@ const
         if lower(s)='sp' then
           actasmregister:=NR_STACK_POINTER_REG;
         if lower(s)='fp' then
-          actasmregister:=NR_STACK_POINTER_REG;
+          actasmregister:=NR_FRAME_POINTER_REG;
         if actasmregister<>NR_NO then
           begin
             result:=true;

rtl/m68k/setjump.inc
@@ -56,7 +56,13 @@ Procedure fpc_longJmp (Var S : Jmp_buf; value : longint); assembler;nostackframe
 asm
   // load S to a0
   move.l 4(sp),a0
-
+  // load 'value' to d0
+  move.l 8(sp),d0
+  // don't return zero
+  tst.l  d0
+  seq    d1
+  and.l #1,d1
+  or    d1,d0
   // restore FP
   move.l (a0),fp
   // restore SP