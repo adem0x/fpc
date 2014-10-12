dc15fc3e1ebd27c1ab6d715fcd3cdf46afb97db2
svenbarth <svenbarth@3ad0048d-3df7-0310-abae-a5850022a9f2>
2014-10-12 18:28:33 +0000

    Creating a branch for a Proof of Concept implementation for Automatic Reference Counted Objects
    git-svn-id: http://svn.freepascal.org/svn/fpc/branches/svenbarth@28800 3ad0048d-3df7-0310-abae-a5850022a9f2

---
+++ trunk/packages/openssl/fpmake.pp
@@ -0,0 +1,34 @@
+{$ifndef ALLPACKAGES}
+{$mode objfpc}{$H+}
+program fpmake;
+
+uses fpmkunit;
+
+Var
+  P : TPackage;
+  T : TTarget;
+begin
+  With Installer do
+    begin
+{$endif ALLPACKAGES}
+
+    P:=AddPackage('openssl');
+{$ifdef ALLPACKAGES}
+    P.Directory:=ADirectory;
+{$endif ALLPACKAGES}
+    P.Version:='2.7.1';
+    P.SourcePath.Add('src');
+    P.OSes := AllUnixOSes+AllWindowsOSes+[OS2,EMX]-[qnx];
+    P.Dependencies.Add('rtl-extra',[OS2,EMX]);
+
+    T:=P.Targets.AddUnit('openssl.pas');
+    T:=P.Targets.AddUnit('fpopenssl.pp');
+
+    P.ExamplePath.Add('examples');
+    P.Targets.AddExampleProgram('test1.pas');
+
+{$ifndef ALLPACKAGES}
+    Run;
+    end;
+end.
+{$endif ALLPACKAGES}
